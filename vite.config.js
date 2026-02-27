60d938e8c74635b8969a5ee94b6cda2fb55585e9
azizaldhubri <azizaldhubri@gmail.com>
2026-02-27 22:41:15 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "/human_resurce/",
+   // base: "/human_resurce/",
   plugins: [react()],
    resolve: {
     dedupe: ['react', 'react-dom']
